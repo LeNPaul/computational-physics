d3c33cd8851cd3623aac71d1e709750564187543
Paul Le <l.nguyen.paul@gmail.com>
2016-09-15 15:21:05 -0400

    Update root.c to include Taylor expansion of root function

--- HW1/EX2/root.c
+++ HW1/EX2/root.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
-int Sq_Tlr(int x)
+int Sq_Tlr(float x)
 {
-	printf("%i \n", x);
+	//The Taylor series is computed with a = 1 for the first 5 terms
+
+	float root = 2/3;
+
+	//float root = 1 + (x - 1) / 2 - ((x - 1) * (x - 1)) / 8 + ((x - 1) * (x - 1) * (x - 1)) / 16 - (5 * (x - 1) * (x - 1) * (x - 1) * (x - 1)) / 128;
+
+	return root;
 }
 
 int main() 
-{
-	Sq_Tlr(4);
+{ 	
+	float value = Sq_Tlr(2);	
+
+	printf("%f \n" , value);
 }
