6322c8fb7add2e11f2333244e7d856173aea4a2b
Paul Le <l.nguyen.paul@gmail.com>
2016-09-19 17:28:57 -0400

    Add Bessel function

---
+++ HW1/EX3.3/bessel.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    float x = 0.5;
+    float n = 20;
+
+    float j5 = 0;
+    float j4 = 1;
+    float j;
+
+    int i, temp;
+
+    for(i = 2; i <= 20; ++i){
+        j = 2 * n * j1/ x - j0;
+
+        j0 = j1;
+        j1 = j;
+    }
+
+    printf("%f \n", j);
+
+    return j;
+} 
