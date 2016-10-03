92129a679afe40ba82110d49d81f6b5cf1c0beb5
Paul Le <l.nguyen.paul@gmail.com>
2016-10-03 17:51:57 -0400

    Update HW2 EX3 files

--- HW2/EX3/main.cc
+++ HW2/EX3/main.cc
@@ -9,13 +9,15 @@ using namespace chrono;
 https://breakingmuscle.com/olympic-weightlifting/the-free-4-week-stronger-than-ever-weightlifting-program
 https://www.t-nation.com/training/olympic-lifting-made-simple
 http://www.alltheheavylifting.com/five-online-olympic-weightlifting-beginner-programs/
+
+http://farside.ph.utexas.edu/teaching/329/lectures/node110.html
 */
 
 int main()
 {
 
 	//Initializing important variables
-	int L = 10; //Latice size
+	int L = 8; //Latice size
 	int lattice[L][L]; //Storing the lattice
 
 	typedef high_resolution_clock myclock;
@@ -34,21 +36,35 @@ int main()
 
 	uniform_real_distribution<double> distribution(0,1.0);
 
-	double coinToss = distribution(generator);
+	//Initializing the lattice
 
 	int i,j;
 	for(i=0; i < L; ++i){
 		for(j=0; j < L; ++j){
 			if(distribution(generator) < 0.5){
 				lattice[i][j] = 1;
-				printf("Less than \n");
+				printf("1 ");
 			}
 			else
 			{
 				lattice[i][j] = 0;
-				printf("Greater than \n");
+				printf("0 ");
 			}
 		}
+		printf("\n");
+	}
+
+	//Determining the energy of the system
+	
+	double totalEnergy = 0; //Setting the energy of the system
+
+
+	//How to increment so that I don't double count
+	for(i=0; i < L; i+=2){
+		for(j=0; j < L; j+=2){
+			printf("%i", lattice[i][j]);
+		}
+		printf("\n");
 	}
 
 }
