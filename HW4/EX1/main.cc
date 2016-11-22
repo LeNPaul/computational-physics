f7af118ce6381c83f0b2aa56261ef776dd861ee8
Paul Le <l.nguyen.paul@gmail.com>
2016-11-22 01:38:22 -0500

    HW4 updates

--- HW4/EX1/main.cc
+++ HW4/EX1/main.cc
@@ -17,16 +17,25 @@ int N = 10000;
 
 //Set the boundary sizes
 double x_left = -100; //Left boundary
-double x_right =  100; //Right boundary
+double x_right = 100; //Right boundary
 
 //Determine the step size
 double h = (x_right - x_left) / N;
 
 int i_match = 0;
 
-vector <double> phi_left(N + 1); //wave function integrating from left
-vector <double> phi_right(N + 1); //wave function integrating from right
-vector <double> phi(N + 1); //Total wave function
+//Wave function integrating from left to right
+vector <double> phi_left(N + 1); 
+
+//Wave function integrating from right to left
+vector <double> phi_right(N + 1);
+
+//Total wave function
+vector <double> phi(N + 1); 
+
+/*
+    The following are potential functions that can be chosen by changing one of the functions to double V(double x)
+*/
 
 double V(double x)
 {
@@ -46,7 +55,7 @@ double V2(double x)
 	//This is the potential for 1.6
 	if(x < pi / 2 && x > 0)
 	{
-		return 0.05 / sin(x) * sin(x) + 5 / cos(x) * cos(x);
+		return (hbar * hbar / (2 * m)) * (0.05 / (sin(x) * sin(x)) + 5 / (cos(x) * cos(x)));
 	}
 	else
 	{
@@ -57,14 +66,11 @@ double V2(double x)
 double V3(double x)
 {
 	//This is the potential for the hint that was given
-	if(x < 5 && x > -5)
-	{
-		return x * x / 5 - 14;
-	}
-	else
-	{
-		return 0;
-	}
+
+    double lambda = 10;
+    
+    return (hbar * hbar / (2 * m)) * (lambda * (lambda - 1)) * (1 / 2 - 1 / (cosh(x) * cosh(x)));
+
 }
 
 //The k value
@@ -78,11 +84,13 @@ double kappa(double x)
 double F(double E)
 {
     //Use the global E value
-    ::E= E;
-    double x = x_right; //start at right boundary
+    ::E = E;
+    
+    //First find the match point starting from the right boundary, and increment to the left using steps of size h
+    double x = x_right;
     i_match = N;
     
-    while(V(x) > E) //in forbiden regon
+    while(V(x) > E)
     {
         --i_match;
         x-= h;
@@ -93,7 +101,9 @@ double F(double E)
         }
     }
 
-    // integrate phi_left using Numerov algorithm
+    //Integrate from left to right using Numerov algorithm for phi_left
+    
+    //Set the first value to 0 and the second value to 1E-10
     phi_left[0] = 0;
     phi_left[1] = 1e-10;
 
@@ -103,24 +113,30 @@ double F(double E)
         phi_left[i + 1] = ( 2 * (1 - 5 * h * h * kappa(x) / 12) * phi_left[i] - ( 1 + h * h * kappa(x - h) / 12 ) * phi_left[i - 1] ) / ( 1 + h * h * kappa(x + h) / 12 );
     }
         
-    //integrate from right
+    //Integrate from right to left using the Numerov algorithm for phi_right
+    
+    //We are going from right to left, so start from N
+    //Set the first value to 0 and the second value to 1E-10
     phi[N] = phi_right[N] = 0;
     phi[N-1] = phi_right[N-1] = 1e-10;
 
     for(int i = N - 1; i>= i_match; i--)
     {
-        x= x_right- i*h;
+        x= x_right - i * h;
         phi_right[i - 1] = (2 * (1 - 5 * h * h * kappa(x) / 12) * phi_right[i] - (1 + h * h * kappa(x + h) / 12) * phi_right[i + 1] ) / (1 + h * h * kappa(x - h) / 12);
         
         phi[i-1] = phi_right[i - 1] /= 1 + h * h * kappa(x - h) / 12;
     }
 
     //Rescale the phi_l vector to match the points
+    
+    double scale = phi_right[i_match] / phi_left[i_match];
+    
     for (int i= 0; i <= i_match + 1; i++)
     {
-        phi[i] = phi_left[i] * phi_right[i_match] / phi_left[i_match];
+        phi[i] = phi_left[i] *= scale;
     }
-
+     
     //Problems may occur if a discontinuity occurs in F(E)
     //To prevent this, do the following:
     
@@ -142,7 +158,7 @@ double F(double E)
     {
         nodes = n;
         sign = -sign;
-    }
+    } 
     
     return sign * (phi_right[i_match - 1] - phi_right[i_match + 1] - phi_left[i_match - 1] + phi_left[i_match + 1]) / (2 * h * phi_right[i_match]);
 
@@ -162,7 +178,7 @@ void normalize()
     if (sum != 1)
     {
         double norm = 0;
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < N; ++i)
         {
             norm += phi[i] * phi[i];
         }
@@ -170,9 +186,9 @@ void normalize()
         //norm /= N;
         norm = sqrt(norm);
 
-        for (int i = 0 ; i <N ; i++)
+        for (int i = 0; i < N; ++i)
         {
-            phi[i] /= (norm * h);
+            phi[i] /= (norm);
         }
     }
 }
@@ -184,7 +200,7 @@ double F_symmmetry()
     double dev_sym = 0;
     int interval_sym = 5;
     double cnt = 0;
-    int N_half = N/2;
+    int N_half = N / 2;
 
     for (int i = 0; i <= interval_sym / h; i = i + 1 )
     {
@@ -199,8 +215,11 @@ double F_symmmetry()
 
 double bisection(double  E_b, double  E_a)
 {
+    
+    //At the end of this bisection routine, the function F(E) will be run with the outputed E value
+    
     //Number of iterations before returning a root
-    int i_max = 15;
+    int i_max = 20;
 
     //Use F(E) to find values at boundaries
     double  F_b = F(E_b);
@@ -260,37 +279,32 @@ double bisection(double  E_b, double  E_a)
 int main()
 {
 
-	//cout << " Eigenvalues of the Schroedinger equation\n" << " for V(x) = 0.2 x^2 - 14\n";
-
-    //This is the energy to stop at
-	double E_stop = -12.0;
-    
     //This is the energy to start at
-	double E_start = -14.0;
+	double E_start = -14.0;    
+    
+    //This is the energy to stop at
+	double E_stop = -6.0;
     
     //This is how much to increment the energy
 	double step_E = (E_stop - E_start) / 4000;
 
     //Open the data files for recording observables
-    ofstream myfile_v;
-    myfile_v.open("data_potential.csv");
+    ofstream myfile_potential;
+    myfile_potential.open("data_potential.csv");
     
     ofstream myfile_phi;
     myfile_phi.open("data_phi.csv");
   	
-    ofstream myfile;
-	myfile.open("data_1_5_symm.csv");
+    ofstream myfile_FE;
+	myfile_FE.open("data_FE.csv");    
 
     //Draw the potential for visualization purposes
     for (int i = 0; i <= N; i++)
     {
         double x = x_left + i * h;
-        myfile_v << V(x) << '\n';
+        myfile_potential << V(x) << '\n';
     }
-    
-    //Set up header for data files
-	//myfile << "E" << "," << "F_E" << "," << "F_symm" << "\n";
-
+   
 	double temp_F_E, temp_F_symm;
 
     for (E = E_start; E <= E_stop; E = E + step_E)
@@ -299,61 +313,66 @@ int main()
         temp_F_E = F(E);
         temp_F_symm = F_symmmetry();
 
-        myfile << E << "," << temp_F_E << "," << temp_F_symm << "\n";
+        myfile_FE << E << "," << temp_F_E << "," << temp_F_symm << "\n";
     }
    
-    //This is where the code that records the observables starts
+    /*
+        This is where the code that records the observables starts
+    */
     
-    //cout << "Eigenvalues of the Schroedinger equation\n" << " for V(x) = 0.2 x^2 - 14\n";
-
-    //Set up header for data files
-    //myfile_phi << "E" << "," << "X" << "," << "PHI" << "\n";
-
-    //eigenvalue 1
+    //The following initial guesses were determined by inspection from plotting F(E) and estimating an interval of size 0.4 that contained a root
+    
     vector <double> E_max(6), E_min(6);
+    
+    E_max[0] = -13.3; 
+    E_min[0] = -13.7;
 
-    E_max[0] = -13.54; 
-    E_min[0] = -13.56;
-
-    E_min[1] = -12.7; 
-    E_max[1] = -12.6;
+    E_min[1] = -12.3; 
+    E_max[1] = -12.7;
     
-    E_min[2] = -11.8; 
-    E_max[2] = -11.75;
+    E_min[2] = -11.9; 
+    E_max[2] = -11.5;
     
-    E_min[3] = -10.9; 
-    E_max[3] = -10.8;
+    E_min[3] = -11; 
+    E_max[3] = -10.6;
     
-    E_min[4] = -9.95; 
-    E_max[4] = -9.85;
+    E_min[4] = -10; 
+    E_max[4] = -9.6;
     
-    E_min[5] = -8.95; 
-    E_max[5] = -8.85;
+    E_min[5] = -9; 
+    E_max[5] = -8.6;
     
-    E_min[6] = -7.8; 
-    E_max[6] = -7.7;
-
+    E_min[6] = -8; 
+    E_max[6] = -7.6;    
+    
     //Record all the wavefunctions in one file
     
-    double sum= 0 ;
+    double sum = 0 ;
     
-    for (int j= 0 ; j <= 6; j++)
+    for (int j = 0; j <= 6; j++)
     {
         //Use the bisection routine to determine the roots
-        E= bisection(E_min[j], E_max[j]);
+        E = bisection(E_min[j], E_max[j]);
 
+        //Normalize the wavefunctions
         normalize();
  
         sum_check();
  
-        for (int i= 0 ; i <= N ; i++)
+        for (int i = 0; i <= N; ++i)
         {
-            myfile_phi << j << "," << x_left + h * i << "," << fabs(phi[i] * phi[i]) << "\n";
+            //Record the eigenfunction and the probability density function
+            myfile_phi << j << "," << x_left + h * i << "," << fabs(phi[i] * phi[i]) << "," << phi[i] << "\n";
         }
+        
+        //Print out the roots/eigenvalues
+        cout << "The " << j << " eigenvalue is: " << E << "\n";
+        
     }
     
+    
     //Close opened data files
-    myfile.close();
+    myfile_FE.close();
     myfile_phi.close();
-    myfile_v.close();
+    myfile_potential.close();
 }
