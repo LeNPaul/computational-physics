7d77d2dbb2b53172007ff843b018f8a8c8485ea7
LeNPaul <l.nguyen.paul@gmail.com>
2017-05-29 00:06:27 -0400

    Add site files

--- HW2/EX1/integrators.h
+++
@@ -1,6 +0,0 @@
-double function(double x);
-double simpleMonte(double a, double b, int N);
-double g_weighting(double x);
-double g_normalized(double x);
-double g_inverse(double u);
-double sampleMonte(double a, double b, int N);
